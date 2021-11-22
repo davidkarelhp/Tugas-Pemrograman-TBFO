365d2f1f7eacb926bf22a2810fc7d88f487a11fe
Brianaldo <brianaldophandiarta31@gmail.com>
2021-11-22 12:54:08 +0700

    add readCNF

---
+++ readCNF.py
@@ -0,0 +1,34 @@
+def readCNF(CNFpath):
+    file = open(CNFpath).read()
+    grammars = file.split('\n')
+    non_terminals = []
+    terminals = []
+    R = {}
+    
+    for grammar in grammars:
+        left, right = grammar.split(" -> ")
+        
+        if left not in non_terminals:
+            non_terminals.append(left)
+            R[left] = []
+
+        right = right.split(" | ")
+        for ri in right:
+            r = ri.split()
+            if (len(r) == 1):
+                if r not in terminals:
+                    [r] = r
+                    terminals.append(r)
+            R[left].append([ri])
+
+    return non_terminals, terminals, R
+
+def main():
+    path = "./CNF.txt"
+    non_terminals, terminals, R = readCNF(path)
+    print("non_terminals =", non_terminals)
+    print("terminals =", terminals)
+    print("R =", R)
+
+if __name__ == "__main__":
+    main()
