02cb6bfeed1177e7230c195e2ec25dbeb52dc68e
davidkarelhp <13520154@std.stei.itb.ac.id>
2021-11-24 15:19:01 +0700

    Update Algoritma

--- src/tubes.py
+++ src/tubes.py
@@ -25,60 +25,76 @@ non_terminals, terminals, CNFgrammar = readCNF.readCNF(r".\src\grammar\CNF.txt")
 
 def cyk(word):
     n = len(word)
-    table = [[set() for j in range(n)] for i in range(n)]
-    array = []
-    singleQuote = True
-    doubleQuote = True
-    for i in range(n):
-        for key in CNFgrammar:
-            for j in range(len(CNFgrammar[key])):
-                if (len(CNFgrammar[key][j]) == 1 and CNFgrammar[key][j][0] == word[i]):
-                    table[i][i].add(key)
-        if not (word[i] in terminals):
-            table[i][i].add("EXPRESSION")
+    if (n == 0):
+        print("accepted")
+        return True
+    else:
+        table = [[set() for j in range(n)] for i in range(n)]
+        array = []
+        singleQuote = True
+        doubleQuote = True
+        for i in range(n):
+            for key in CNFgrammar:
+                for j in range(len(CNFgrammar[key])):
+                    if (len(CNFgrammar[key][j]) == 1 and CNFgrammar[key][j][0] == word[i]):
+                        table[i][i].add(key)
+            if not (word[i] in terminals):
+                table[i][i].add("EXPRESSION")
 
-        # Tidak membaca komen dan string
-        if (word[i] == "'"):
-            singleQuote = not singleQuote
-        if (word[i] == '"'):
-            doubleQuote = not doubleQuote
-
-        if singleQuote and doubleQuote and (not (word[i] in terminals) or word[i] == "pass" or word[i] == "break" or word[i] == "continue" or word[i] == "None"):
-            array.append(word[i])
-        else:
-            # if (len(array) != 0):
-            #     print(array) 
-            # Panggil fungsi di sini di baris ini kalau len(array) != 0
-            array = []
-
-    # printTable(table)
-
-    # count = 0
-    accepted = False
-    for l in range(1, n + 1):
-        for i in range(n - l + 1):
-            j = i + l - 1
-            for k in range(i, j):
-                for key in CNFgrammar:
-                    for x in range(len(CNFgrammar[key])):
-                        # count += 1
-                        if (len(CNFgrammar[key][x]) == 2):
-                            if (CNFgrammar[key][x][0] in table[i][k] and CNFgrammar[key][x][1] in table[k + 1][j]):
-                                    table[i][j].add(key)
-                        if (i == 0 and j == n - 1 and "S" in table[0][n - 1]):
-                            accepted = True
+            # Tidak membaca komen dan string
+            if (word[i] == "'"):
+                singleQuote = not singleQuote
+            if (word[i] == '"'):
+                doubleQuote = not doubleQuote
+            # print(singleQuote, doubleQuote, (not (word[i] in terminals)))
+            # print(word[i])
+            if singleQuote and doubleQuote and ((not (word[i] in terminals)) or word[i] == "pass" or word[i] == "break" or word[i] == "continue" or word[i] == "None"):
+                array.append(word[i])
+            else:
+                # if (len(array) != 0):
+                #     print("lol")
+                #     print(array) 
+                    
+                # Panggil fungsi di sini di baris ini kalau len(array) != 0
+                array = []
+        if (len(array) != 0):
+            pass
+            # print("lol")
+            # print(array)
+        check = True
+        for i in range(n):
+                if not ("EXPRESSION" in table[i][i] or "BOOLEAN" in table[i][i]):
+                    check = False
+                    break
+        if (check):
+            print("accepted")
+            return True
+        # count = 0
+        accepted = False
+        for l in range(1, n + 1):
+            for i in range(n - l + 1):
+                j = i + l - 1
+                for k in range(i, j):
+                    for key in CNFgrammar:
+                        for x in range(len(CNFgrammar[key])):
+                            # count += 1
+                            if (len(CNFgrammar[key][x]) == 2):
+                                if (CNFgrammar[key][x][0] in table[i][k] and CNFgrammar[key][x][1] in table[k + 1][j]):
+                                        table[i][j].add(key)
+                            if (i == 0 and j == n - 1 and "S" in table[0][n - 1]):
+                                accepted = True
+                                break
+                        if (accepted):
                             break
                     if (accepted):
-                        break
-                if (accepted):
-                    break                               
-    print(table[0][n - 1])
-    # print(count)
-    if (accepted):
-        print("accepted")
-    else:
-        print("rejected")
-    return accepted
+                        break                               
+        # printTable(table)
+        # print(count)
+        if (accepted):
+            print("accepted")
+        else:
+            print("rejected")
+        return accepted
 
 # word = "(((((e))))(e)((e)))"
 
