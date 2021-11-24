e5d5b541701abcb52c50616567f1adf92d2965e0
Aldwin Hardi Swastia <ahardiswastia@gmail.com>
2021-11-24 12:08:56 +0700

    Update cobacobaFA.py

--- src/cobacobaFA.py
+++ src/cobacobaFA.py
@@ -2,6 +2,7 @@
 def varCheck(word):
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     correct = False
+    datatype = "REJECT"
     if word == "True":
         datatype = "NONASSIGN"
         correct = True
@@ -69,9 +70,20 @@ def varCheck(word):
         datatype = "RELATIONS"
         correct = True
     
-    elif not(word[0] in numbers):
+    elif ((ord(word[0]) > 64 and ord(word[0]) < 91) or (ord(word[0]) == 95) or (ord(word[0]) > 96 and ord(word[0]) < 123)):
         datatype = "VARIABLES"
         correct = True
 
+    else:
+        angka = True
+        for huruf in word:
+            if not(huruf in numbers):
+                angka = False
+                break
+        if(angka):
+            datatype = "NONASSIGN"
+            correct = True
+
     return correct, datatype
+    
     
