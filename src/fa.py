8c4b7951bc57cf38c9b6e19cfec46c17f0a8a771
davidkarelhp <13520154@std.stei.itb.ac.id>
2021-11-24 19:20:09 +0700

    Update FA

--- src/fa.py
+++ src/fa.py
@@ -7,7 +7,8 @@ COMP = ['==', '<=', '>=', '!=', '<', '<']
 def NUM(str):
     return str.isnumeric()
 ASS = ['=']
-FINALSTATES = ['S', 'A', 'B', 'C', 'D']
+FINALSTATES = ['S', 'A', 'B', 'C', 'D', 'F', 'H', 'J']
+
 def fa(array):
     current_state = 'S'
     n = len(array)
@@ -30,23 +31,23 @@ def fa(array):
             else:
                 return False
         elif (current_state == 'A'):
-            if (array[i] in OP or array[i] in ASS):
-                return False
+            if (array[i] in OP or array[i] in COMP):
+                current_state = 'E'
             elif (NUM(array[i])):
                 current_state = 'A'
             elif (array[i] in NA):
                 current_state = 'C'
             elif (array[i] in BN):
                 current_state = 'D'
-            elif (array[i] in COMP):
-                current_state = 'E'
             elif (VV(array[i])):
                 current_state = 'B'
             else:
                 return False
         elif (current_state == 'B'):
-            if (array[i] in OP or array[i] in COMP or array[i] in ASS):
+            if (array[i] in OP or array[i] in COMP):
                 current_state = 'E'
+            elif (array[i] in ASS):
+                current_state = 'I'
             elif (NUM(array[i])):
                 current_state = 'A'
             elif (array[i] in NA):
@@ -58,9 +59,7 @@ def fa(array):
             else:
                 return False
         elif (current_state == 'C'):
-            if (array[i] in OP or array[i] in COMP or array[i] in ASS):
-                return False
-            elif (NUM(array[i])):
+            if (NUM(array[i])):
                 current_state = 'A'
             elif (array[i] in NA):
                 current_state = 'C'
@@ -71,33 +70,44 @@ def fa(array):
             else:
                 return False
         elif (current_state == 'D'):
-            if (array[i] in ASS):
-                return False
+            if (array[i] in OP or array[i] in COMP):
+                current_state = 'E'
             elif (NUM(array[i])):
                 current_state = 'A'
             elif (array[i] in NA):
                 current_state = 'C'
             elif (array[i] in BN):
                 current_state = 'D'
-            elif (array[i] in OP or array[i] in COMP):
-                current_state = 'E'
             elif (VV(array[i])):
                 current_state = 'B'
             else:
                 return False
         elif (current_state == 'E'):
-            if (array[i] in OP or array[i] in COMP or array[i] in ASS):
-                return False
-            elif (NUM(array[i])):
-                current_state = 'A'
-            elif (array[i] in NA):
-                current_state = 'C'
-            elif (array[i] in BN):
-                current_state = 'D'
-            elif (VV(array[i])):
-                current_state = 'B'
+            if (VV(array[i]) or NUM(array[i]) or array[i] in BN):
+                current_state = 'F'
             else:
                 return False
+        elif (current_state == 'F'):
+            if (array[i] in OP or array[i] in COMP):
+                current_state = 'G'
+            else:
+                return False
+        elif (current_state == 'G'):
+            if (VV(array[i]) or NUM(array[i]) or array[i] in BN):
+                current_state = 'H'
+            else:
+                return False
+        elif (current_state == 'I'):
+            if (VV(array[i]) or NUM(array[i]) or array[i] in BN):
+                current_state = 'J'
+            else:
+                return False
+        elif (current_state == 'J'):
+            if (array[i] in OP or array[i] in COMP or array[i] in ASS):
+                current_state = 'G'
+            else:
+                return False
+
     # print('state')
     # print("endstate " + current_state)
     return current_state in FINALSTATES
